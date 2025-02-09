2a32f619e46f5bcff85739024a0d168f296564b4
kakon-selise <kakon.mehedi@selisegroup.com>
2025-02-09 20:45:35 +0600

    refactor

--- IAM/Services/Implementations/JwtTokenService.cs
+++ IAM/Services/Implementations/JwtTokenService.cs
@@ -36,7 +36,7 @@ public class JwtTokenService : IJwtTokenService
 
     public string GenerateToken(User user)
     {
-        var claims = new List<Claim>
+        List<Claim> claims = new()
         {
             new Claim(JwtRegisteredClaimNames.Sub, user.Email),
             new Claim(AppClaims.Role, user.Role),
@@ -51,7 +51,7 @@ public class JwtTokenService : IJwtTokenService
         return GenerateJwtToken(claims, tokenValidity);
     }
 
-    string GenerateJwtToken(List<Claim> claims, DateTime expiresIn)
+    private string GenerateJwtToken(List<Claim> claims, DateTime expiresIn)
     {
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
